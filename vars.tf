4c7e6cddc0c8bd5b20209c9e1b18729510cc6906
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-05 04:23:23 +0530

    error detected

--- vars.tf
+++ vars.tf
@@ -4,7 +4,7 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
 variable "engine_version" {}
-variable "db_subnet_ids" {}
+variable "subnet_ids" {}
 variable "tags" {}
 variable "storage_encrypted" {
     default = true
